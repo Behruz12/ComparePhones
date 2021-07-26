7227af6855b1c6771878afbdef1ac5e5840c3273
Behruz <jalbeh@icloud.com>
2021-07-26 17:39:45 +0500

    ...

--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView, ListView, DeleteView, View
+from django.views.generic import CreateView, ListView, DeleteView, View
 from .models import Post, Brand, Phones
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from django.http import HttpResponse
+
 
 
 class MainIndex(ListView):
@@ -30,45 +30,9 @@ class CompareOne(View):
         except Phones.DoesNotExist:
             return redirect('main:compare-phones')
 
-        # m = Phones.objects.filter(id=d.id)
         return render(request, "main/compare-one.html", {
             'one': d
         })
-
-# def main_ajax(request):
-#     Brand.objects.all()
-#
-#     # apple = Brand(name="Apple")
-#     # apple.save()
-#     # samsung = Brand(name="Samsung")
-#     # samsung.save()
-#     #
-#     # Brand(parent=apple, name="iPhone 5").save()
-#     # Brand(parent=apple, name="iPhone 6").save()
-#     # Brand(parent=apple, name="iPhone 12").save()
-#     #
-#     # Brand(parent=samsung, name="Galaxy S20").save()
-#     # Brand(parent=samsung, name="Galaxy S21").save()
-#     # Brand(parent=samsung, name="Galaxy S5").save()
-#     #
-#     return render(request, 'widgets/ajax.html', {
-#         'brands': Brand.objects.filter(parent__isnull=True).all()
-#     })
-#
-#
-# def main_child(request, pid):
-#     return HttpResponse("".join(["<option value='{}'>{}</option>".format(row.id, row.name)
-#                                 for row in Brand.objects.filter(parent_id=pid).order_by('id').all()]))
-#
-
-# class CompareIndex(ListView):
-#     model = Phones
-#     template_name = "main/compare.html"
-#
-#     def phone_compare(self, request):
-#         return render(request, {
-#             'phones': Phones.objects.all()
-#         })
 
 
 def ComaprePhones(request):
@@ -92,34 +56,6 @@ def Compare_Phones(request, i1, i2):
 
     })
 
-# class PhoneIndex(ListView):
-#     model = Phones
-#     template_name = 'main/compare.html'
-#
-#
-#     def get_queryset(self):
-#         queryset = super().get_queryset()
-#         return queryset.filter(id=id)
-
-
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    fields = ('subject', 'content', 'image')
-
-    def setup(self, request, *args, **kwargs):
-        super().setup(request, *args, **kwargs)
-
-        request.title = "Maqola qo'shish"
-
-    def form_valid(self, form):
-        post = form.save(commit=False)
-        post.user_id = self.request.user.id
-        post.save()
-
-        messages.success(self.request, "Maqola muvaffaqiytli qo'shildi.")
-
-        return redirect('main:index')
-
 
 class PhoneCreateView(LoginRequiredMixin, CreateView):
     model = Phones
@@ -138,8 +74,6 @@ class PhoneCreateView(LoginRequiredMixin, CreateView):
         messages.success(self.request, "Maqola muvaffaqiytli qo'shildi.")
 
         return redirect('main:index')
-
-
 
 
 class PostDeleteView(DeleteView):
